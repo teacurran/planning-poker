5ef867e58acda915db8a20a2c23b71da5c8b9ab7
Terrence Curran <tcurran@approachingpi.com>
2023-01-29 00:27:05 -0500

    crud

--- server/src/main/java/com/terrencecurran/planningpoker/data/model/Room.java
+++ server/src/main/java/com/terrencecurran/planningpoker/data/model/Room.java
@@ -1,5 +1,7 @@
 package com.terrencecurran.planningpoker.data.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,32 +12,66 @@ public class Room {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
-  private String firstName;
-  private String lastName;
+
+  private String code;
+  private String name;
+  private LocalDateTime dateCreated;
+  private LocalDateTime dateActive;
 
   protected Room() {}
 
-  public Room(String firstName, String lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+  public Room(String code, String name, LocalDateTime dateCreated, LocalDateTime dateActive) {
+    this.code = code;
+    this.name = name;
+    this.dateCreated = dateCreated;
+    this.dateActive = dateActive;
   }
 
   @Override
   public String toString() {
     return String.format(
-        "Customer[id=%d, firstName='%s', lastName='%s']",
-        id, firstName, lastName);
+        "Room[id=%d, code='%s', name='%s', dateCreated='%s', dateActive='%s']",
+        id, code, name, dateCreated, dateActive);
   }
 
   public Long getId() {
     return id;
   }
 
-  public String getFirstName() {
-    return firstName;
+  public void setId(Long id) {
+    this.id = id;
   }
 
-  public String getLastName() {
-    return lastName;
+  public String getCode() {
+    return code;
   }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDateTime getDateCreated() {
+    return dateCreated;
+  }
+
+  public void setDateCreated(LocalDateTime dateCreated) {
+    this.dateCreated = dateCreated;
+  }
+
+  public LocalDateTime getDateActive() {
+    return dateActive;
+  }
+
+  public void setDateActive(LocalDateTime dateActive) {
+    this.dateActive = dateActive;
+  }
+  
 }
